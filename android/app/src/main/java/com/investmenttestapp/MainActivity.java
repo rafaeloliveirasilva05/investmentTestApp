2a4571cb407d12e70ad017fd83b7a16a6576d47d
rafael05 <rafaeloliveirasilva05@gmail.com>
2019-09-11 16:24:39 -0300

    rename application

--- android/app/src/main/java/com/investmenttestapp/MainActivity.java
+++ android/app/src/main/java/com/investmenttestapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.testealtran;
+package com.investmenttestapp;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "testeAltran";
+        return "investmentTestApp";
     }
 
     @Override
